bb62c7f1009578ec3d254a5374beb1e3458fc62e
David Mc Ken <david.mcken@gmail.com>
2023-08-27 12:47:42 +1000

    Add airosv8.py Initial commit.

---
+++ src/ubnt_automata/airosv8.py
@@ -0,0 +1,215 @@
+'''Ubnt v8 handler.
+'''
+
+# System imports
+import json
+import logging
+import sys
+import traceback
+import urllib3
+
+# External imports
+import requests
+
+# Local imports
+from . import exceptions
+
+logger = logging.getLogger(__name__)
+
+# Disable the self signed certificate warnings.
+urllib3.disable_warnings()
+
+
+class AirOSv8:
+    '''Ubnt version 8 equipment handler.
+
+
+    To implement:
+    - /test_mode.cgi
+    - /airviewdata.cgi
+    - /chanlist_active.cfg
+    - /survey.json.cgi?iface=ath0&update=last - Site survey
+    - /amdata.cgi
+    '''
+
+    def __init__(self, host: str, password: str, username: str='ubnt',
+                 autologin:bool=False) -> None:
+        '''Constructor'''
+        self._req_session = requests.Session()
+        self._host = host
+        self._username = username
+        self._password = password
+        self._is_ssl = True
+
+        if autologin:
+            self.login_http(self._password)
+
+    def _build_url(self, path: str):
+        '''Build the final URL to pass to request library'''
+        return "{0}://{1}/{2}".format(
+            "https" if self._is_ssl else "http",
+            self._host,
+            path,
+        )
+
+    def login_http(self, password:str, username:str='ubnt'):
+        '''Login to device via HTTP(s).
+
+        '''
+        try:
+            auth_data = {
+                'username': username,
+                'password': password,
+            }
+
+            # Get cookies
+            self._req_session.get(
+                self._build_url(''),
+                verify=False
+            )
+
+            rez = self._req_session.post(
+                self._build_url("api/auth"),
+                data=auth_data,
+                verify=False,
+            )
+
+            if rez.status_code != 200:
+                logger.debug(f"Error logging in: {rez.json()['error']}")
+                raise exceptions.WrongPassword()
+
+            # Successful login
+            self._curr_password = password
+            self._dev_info = rez.json()['boardinfo']
+            self._csrf_id = rez.headers['X-CSRF-ID']
+        except exceptions.WrongPassword:
+            raise
+        except Exception as exc:
+            logger.debug(
+                f"An exception occurred 'login_http' - {self._host}: " +
+                f"{exc.__class__} -> {exc}"
+            )
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            logger.debug(repr(
+                traceback.format_exception(exc_type, exc_value, exc_traceback)
+            ))
+            raise
+
+    def change_password(self, new_pw, old_pw=None, username = 'ubnt'):
+        '''Change current user password.
+        '''
+
+        if old_pw is None:
+            old_password = self._curr_password
+        else:
+            old_password = old_pw
+
+        pw_data = {
+            'change': 'yes',
+            'ro': '0',
+            'pwd': new_pw,
+            'oldPwd': old_password,
+        }
+        rez = self._req_session.post(
+            self._build_url("pwd.cgi"),
+            data=pw_data,
+            verify=False,
+            headers = {
+                'Accept': 'application/json, text/javascript, */*; q=0.01',
+                'X-CSRF-ID': self._csrf_id,
+            }
+        )
+
+        try:
+            change_result = rez.json()
+
+            if change_result['success'] is True:
+                return
+
+            logger.error(f"Error changing password: {change_result}")
+        except json.decoder.JSONDecodeError:
+            logger.error(
+                f"Error decoding json in change password: {rez.content}"
+            )
+
+
+    def getcfg(self):
+        '''Get the device configuration.
+
+
+        '''
+        rez = self._req_session.get(
+            self._build_url("getcfg.cgi"),
+            verify=False,
+            headers = {
+                'X-CSRF-ID': self._csrf_id,
+            }
+        )
+
+        full_cfg = rez.text
+
+        cfg_data = {}
+        for curr_line in full_cfg.split('\n'):
+            try:
+                key,val = curr_line.strip().split('=',1)
+                cfg_data[key] = val
+            except ValueError:
+                logger.error(f"Unable to parse line: {curr_line}")
+
+        return cfg_data
+
+    def writecfg(self, cfgdata):
+        '''Write config to device.
+        '''
+        lines = []
+        for k,val in cfgdata.items():
+            lines.append(f"{k}={val}")
+
+        pprint.pprint(lines)
+
+        cfg_output = "\r\n".join(lines)
+
+        print(cfg_output)
+
+        cfg_data = {
+            'cfgData': cfg_output,
+        }
+        rez = self._req_session.post(
+            self._build_url("writecfg.cgi"),
+            data=cfg_data,
+            verify=False,
+            headers = {
+                'X-CSRF-ID': self._csrf_id,
+            }
+        )
+
+        res_data = rez.json()
+        if res_data['ok'] is True:
+            # Change was successful
+            return
+
+        logger.error(f"Error changing config: {res_data}")
+
+    def getstatus(self):
+        '''Get the device status.
+
+
+        '''
+        res = self._req_session.get(
+            self._build_url("status.cgi"),
+            verify=False,
+            headers = {
+                'X-CSRF-ID': self._csrf_id,
+            }
+        )
+
+        if res.status_code == 200:
+            return res.json()
+        else:
+            raise RuntimeError(f"Error fetching status: {res.text}")
+
+if __name__ == '__main__':
+    import http.client
+    import pprint
+
+    http.client.HTTPConnection.debuglevel = 1
