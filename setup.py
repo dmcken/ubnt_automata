17ee7097ac9dca47f5cd6f13c0a390d9ba0a2458
David Mc Ken <david.mcken@gmail.com>
2023-08-27 12:52:16 +1000

    Update setup.py & pyproject.toml Update ubnt-automata to ubnt_automata

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name="ubnt-automata",  # Required
+    name="ubnt_automata",  # Required
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
@@ -65,7 +65,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url="https://github.com/dmcken/ubnt-automata",  # Optional
+    url="https://github.com/dmcken/ubnt_automata",  # Optional
     # This should be your name or the name of the organization which owns the
     # project.
     author="David M.",  # Optional
@@ -162,9 +162,9 @@ setup(
     # maintainers, and where to support the project financially. The key is
     # what's used to render the link text on PyPI.
     project_urls={  # Optional
-        "Bug Reports": "https://github.com/dmcken/ubnt-automata/issues",
+        "Bug Reports": "https://github.com/dmcken/ubnt_automata/issues",
         # "Funding":     "https://donate.pypi.org",
         # "Say Thanks!": "http://saythanks.io/to/example",
-        "Source": "https://github.com/dmcken/ubnt-automata/",
+        "Source": "https://github.com/dmcken/ubnt_automata/",
     },
 )
