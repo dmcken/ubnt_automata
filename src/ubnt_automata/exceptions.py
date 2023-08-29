b43101114d70ee36ca824259c0cef000b75b14d5
David Mc Ken <david.mcken@gmail.com>
2023-08-30 06:11:39 +1000

    Update exceptions.py Add DeviceUnavailable exception.

--- src/ubnt_automata/exceptions.py
+++ src/ubnt_automata/exceptions.py
@@ -1,4 +1,7 @@
 '''General exceptions'''
 
+
+class DeviceUnavailable(Exception):
+    '''Exception to handle a device being unavailable'''
 class WrongPassword(Exception):
     '''Exception to handle a wrong password'''
