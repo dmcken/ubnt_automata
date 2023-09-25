7759a73d71fdbf901e25fbb602312d7d559abf5f
David Mc Ken <david.mcken@gmail.com>
2023-09-25 18:57:51 -0400

    Update __init__.py Add determine_device_type as module entry.

--- src/ubnt_automata/__init__.py
+++ src/ubnt_automata/__init__.py
@@ -8,10 +8,12 @@ from . import exceptions
 # Classes
 from .airosv8 import AirOSv8
 from .airosv6 import AirOSv6
+from .utils import determine_device_type
 
 
 __all__ = [
     'AirOSv6',
     'AirOSv8',
+    'determine_device_type',
     'exceptions',
 ]
