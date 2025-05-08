baed7362355b2235fbc20ec1f402d3f65af98a91
David Mc Ken <david.mcken@gmail.com>
2025-05-08 07:50:34 -0400

    Add airfiber5x module (just notes).

---
+++ src/ubnt_automata/airfiber5x.py
@@ -0,0 +1,19 @@
+'''AirFiber 5X and 5X HD implementation.
+
+Auth endpoint:
+- /api/auth
+
+Data endpoints (as of 1.5.4):
+- airviewdata.cgi
+- antlist.cgi
+- btstatus.cgi
+- getcfg.cgi
+- hist-stats.cgi
+- ipscan.cgi
+- poll.cgi
+- status.cgi
+- /api/v1.0/tools/unms
+- /api/fw/update-check
+- /api/warnings
+
+'''
